d3989815050856e5df6a14484da8bd03497d3511
YubrajKarki <yubrajkarki528@gmail.com>
2021-08-18 15:22:09 +0545

    void keyword

--- Main.java
+++ Main.java
@@ -14,20 +14,33 @@
 
 // JAVA METHOD
 // public class Main {
-// // JAVA METHOD
-// static void myMethod(String fname, int age) {
-// // code to be executed
-// System.out.println("Hi, my name is " + fname + " and I am " + age + " years
-// old.");
-// }
+//     // JAVA METHOD
+//     static void myMethod(String fname, int age) {
+//         // code to be executed
+//         System.out.println("Hi, my name is " + fname + " and I am " + age + " years old.");
+//     }
 
-// // EXECUTING THE METHOD
-// public static void main(String[] args) {
-// // PASSING ARGUMENTS TO THE METHOD
-// myMethod("Joseph", 30);
-// }
+//     // EXECUTING THE METHOD
+//     public static void main(String[] args) {
+//         // PASSING ARGUMENTS TO THE METHOD
+//         myMethod("Joseph", 30);
+//     }
 
 // }
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// VOID
+public class Main {
+    // Void keyword is omitted so the method should return some value
+    static int myMethod(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int z = myMethod(10, 12);
+        System.out.println(z);
+    }
+
+}
