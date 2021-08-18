a25c81e98436bdf61a53aa3f3765a7d19b5cb8fe
YubrajKarki <yubrajkarki528@gmail.com>
2021-08-18 15:29:37 +0545

    IF ELSE inside JAVA MEHTODS

--- Main.java
+++ Main.java
@@ -14,17 +14,18 @@
 
 // JAVA METHOD
 // public class Main {
-//     // JAVA METHOD
-//     static void myMethod(String fname, int age) {
-//         // code to be executed
-//         System.out.println("Hi, my name is " + fname + " and I am " + age + " years old.");
-//     }
+// // JAVA METHOD
+// static void myMethod(String fname, int age) {
+// // code to be executed
+// System.out.println("Hi, my name is " + fname + " and I am " + age + " years
+// old.");
+// }
 
-//     // EXECUTING THE METHOD
-//     public static void main(String[] args) {
-//         // PASSING ARGUMENTS TO THE METHOD
-//         myMethod("Joseph", 30);
-//     }
+// // EXECUTING THE METHOD
+// public static void main(String[] args) {
+// // PASSING ARGUMENTS TO THE METHOD
+// myMethod("Joseph", 30);
+// }
 
 // }
 
@@ -32,15 +33,34 @@
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // VOID
+// public class Main {
+// // Void keyword is omitted so the method should return some value
+// static int myMethod(int x, int y) {
+// return x + y;
+// }
+
+// public static void main(String[] args) {
+// int z = myMethod(10, 12);
+// System.out.println(z);
+// }
+
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// IF ELSE INSIDE METHODS
 public class Main {
-    // Void keyword is omitted so the method should return some value
-    static int myMethod(int x, int y) {
-        return x + y;
+    static void verifyAuthority(String name, int age) {
+        if (age > 18)
+            System.out.println("Hi, " + name + ", you have this right");
+        else
+            System.out.println("Hi, " + name + ", you don't have a right for this.");
+
     }
 
     public static void main(String[] args) {
-        int z = myMethod(10, 12);
-        System.out.println(z);
+        verifyAuthority("John", 17);
+        verifyAuthority("Anna", 20);
     }
-
 }
