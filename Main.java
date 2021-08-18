90b5644b40120330904a0746ca23adc229ad946b
YubrajKarki <yubrajkarki528@gmail.com>
2021-08-18 14:14:07 +0545

    Done with the basics

--- Main.java
+++ Main.java
@@ -1,6 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
-        String name = "john";
-        System.out.println("Hello my name is " + name);
-    }
-}
+
+// public class Main {
+// public static void main(String[] args) {
+
+// String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+// System.out.println(typeofcars);
+
+// }
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// JAVA METHOD
+// public class Main {
+// // JAVA METHOD
+// static void myMethod(String fname, int age) {
+// // code to be executed
+// System.out.println("Hi, my name is " + fname + " and I am " + age + " years
+// old.");
+// }
+
+// // EXECUTING THE METHOD
+// public static void main(String[] args) {
+// // PASSING ARGUMENTS TO THE METHOD
+// myMethod("Joseph", 30);
+// }
+
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
